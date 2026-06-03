ac2ad7ae2c7b5ee0005472ebaecf4e57d2babf79
MickelCM89 <miguelfields832@gmail.com>
2026-06-03 13:32:52 +0200

    Ajout filtres genre et pays

--- streamlitapp.py
+++ streamlitapp.py
@@ -365,7 +365,7 @@ st.markdown("""
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
         background-clip: text;
-    ">💥 Top 10 Action</h3>
+    ">💥 Top Action</h3>
 """, unsafe_allow_html=True)
 
 top_action = df_films[df_films['genres'].str.contains('Action', na=False)]\
